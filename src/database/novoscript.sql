6959ff36f7707372a1f337bc1baf16b8fab2a66a
RafaelSampaio777 <rafael.macedo@sptech.school>
2022-05-12 15:07:53 -0300

    Rafael Sampaio - atualização DB

--- src/database/novoscript.sql
+++ src/database/novoscript.sql
@@ -1,75 +1,55 @@
-create database COMPROD;
-use COMPROD;
-
-create table Empresas (idempresa int primary key auto_increment,
-nomeEmpresa varchar(60),
-CNPJ varchar(14),
-email varchar(45),
-senha varchar(16));
-
-
-insert into Empresas (nomeEmpresa,CNPJ, email, senha)
-value ("itau", "01012012000101", 'empresabanco@outlook.com', 'digito');
-
-
-create table Enderecos (idenderecos int primary key auto_increment,
-CEP varchar(20),
-rua varchar(45),
-complemento varchar(10),
-bairro varchar(60),
-cidade varchar(35),
-fk_empresa int,
-foreign key (fk_empresa) references Empresas (idempresa));
-
-insert into Enderecos (CEP, rua, complemento, bairro, cidade, fk_empresa)
-value ("69399-999","jardins", "51", "paulista", 'sp', '01');
-
-
-create table arduino (
-idarduino int primary key auto_increment,
-codigoarduino varchar(50),
-modelo varchar(60),
-setor_instalado varchar(45),
-fk_empresas int,
-foreign key (fk_empresas) references Empresas (idempresa)
+CREATE TABLE Empresas (
+IDempresa INT PRIMARY KEY auto_increment,
+nomeEmpresa VARCHAR(60),
+CNPJ VARCHAR(14)
 );
 
-insert into arduino ( codigoarduino, modelo , setor_instalado, fk_empresas)
-value ("codigoum", 'split', 'area_rural', 01);
+CREATE TABLE salas (
+  IDsala INT NOT NULL AUTO_INCREMENT,
+  andar VARCHAR(45),
+  sala VARCHAR(45),
+  FK_empresa INT NOT NULL,
+  PRIMARY KEY (IDsala),
+FOREIGN KEY (FK_empresa)REFERENCES Empresas (IDempresa)
+);
 
+CREATE TABLE cargos (
+  IDcargo INT NOT NULL AUTO_INCREMENT,
+  nivel VARCHAR(45) NULL,
+  PRIMARY KEY (IDcargo)
+  );
+  
+CREATE TABLE funcionarios (
+IDfuncionario INT NOT NULL AUTO_INCREMENT,
+nomeFuncionario VARCHAR (45),
+CPF CHAR (11),
+dataNascimento DATE,
+email VARCHAR (45),
+senha VARCHAR (16),
+FK_empresa INT,
+FOREIGN KEY (FK_empresa)REFERENCES Empresas (IDempresa),
+FK_cargo INT,
+FOREIGN KEY (FK_cargo)REFERENCES cargos (IDcargo),
+PRIMARY KEY (IDfuncionario, FK_empresa, FK_cargo)
+);
 
-create table historico (idsensor int primary key auto_increment,
-data_hora datetime,
-luminosidade_atual varchar(50),
-temperatura_atual varchar(35),
-temperatura_maxima varchar(40),
-temperatura_minima varchar(80),
-fk_sensor int,
-foreign key (fk_sensor) references arduino(idarduino));
+CREATE TABLE arduino (
+IDarduino INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+modelo VARCHAR (30),
+codigo CHAR (10),
+FK_sala INT,
+FOREIGN KEY (FK_sala) REFERENCES salas (IDsala)
+);
 
-
-insert into historico (data_hora, luminosidade_atual, temperatura_atual, temperatura_maxima, temperatura_minima,fk_sensor)
-value ("2022-01-15 10:10:01", "80", "20", "30", "15", 01);
-
-describe Historico;
-create table Funcionarios (idfuncionario int primary key auto_increment,
-nomeFuncionario varchar(50),
-CPF varchar(90),
-cargo varchar(100),
-dataNascimento varchar(120),
-email varchar(90),
-senha varchar(16),
-fk_terceirafkempresa int,
-foreign key (fk_terceirafkempresa) references Empresas ( idempresa));
-
-alter table Funcionarios add column fk_chefe int;
-alter table Funcionarios add foreign key (fk_chefe) references Funcionarios (idFuncionario);
-select * from Funcionarios;
-
-insert into Funcionarios (nomeFuncionario, CPF, cargo, dataNascimento, email, senha, fk_terceirafkempresa)
-value ("luis", "987654-000", "ajudante", "dezembro", "luis01@gmail.com", "senhaum", 01);
-
-
-update Funcionarios set fk_chefe = null where idfuncionario = 01;
-update Funcionarios set fk_chefe = '01' where idfuncionario = 02;
-update Funcionarios set fk_chefe = '02' where idfuncionario = 03;
+CREATE TABLE historico (
+IDarduino INT auto_increment,
+fk_sensor INT,
+data_hora datetime DEFAULT CURRENT_TIMESTAMP,
+dht11_umidade DECIMAL (10,2),
+dht11_temperatura DECIMAL (10,2),
+luminosidade DECIMAL (10,2),
+lm35_temperatura DECIMAL (10,2),
+chave INT,
+FOREIGN KEY (fk_sensor) REFERENCES arduino(idarduino),
+PRIMARY KEY (IDarduino, fk_sensor)
+);
