81ee7e27e52089938070796c21936d9b4379fe34
RafaelSampaio777 <rafael.macedo@sptech.school>
2022-05-26 14:40:52 -0300

    .

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -13,7 +13,7 @@ function buscarUltimasMedidas(idSala, limite_linhas) {
                     from historico
                     join arduino on arduino.IDarduino = historico.fk_sensor
                     where fk_sala = ${idSala}
-                    order by IDhistorico desc`;
+                    order by IDhistorico desc;`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
         lm35_temperatura as temperatura, 
@@ -23,7 +23,7 @@ function buscarUltimasMedidas(idSala, limite_linhas) {
                         from historico
                         join arduino on arduino.IDarduino = historico.fk_sensor
                         where fk_sala = ${idSala}
-                    order by IDhistorico desc limit ${limite_linhas}`;
+                    order by IDhistorico desc limit ${limite_linhas};`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -41,21 +41,21 @@ function buscarMedidasEmTempoReal(idSala) {
         instrucaoSql = `select top 1
         lm35_temperatura as temperatura, 
         luminosidade as luminosidade,  
-                        CONVERT(varchar, data_hora, 108) as momento_grafico, 
+                        CONVERT(varchar, data_hora, 108) as momento_grafico 
                         from historico
                         join arduino on arduino.IDarduino = historico.fk_sensor
                         where fk_sala = ${idSala}
-                    order by IDhistorico desc`;
+                    order by IDhistorico desc;`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
         lm35_temperatura as temperatura, 
         luminosidade as luminosidade,
-                        DATE_FORMAT(data_hora,'%H:%i:%s') as momento_grafico, 
+                        DATE_FORMAT(data_hora,'%H:%i:%s') as momento_grafico 
                         from historico
                         join arduino on arduino.IDarduino = historico.fk_sensor
                         where fk_sala = ${idSala}
-                    order by IDhistorico desc limit 1`;
+                    order by IDhistorico desc limit 1;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
