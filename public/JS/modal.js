d73c1e992f4f4455a94791067ffd99d91c968432
RafaelSampaio777 <rafael.macedo@sptech.school>
2022-06-01 08:11:04 -0300

    modals funcionando

---
+++ public/JS/modal.js
@@ -0,0 +1,20 @@
+function saibaMais(){
+    window.location = `index.html#bg`;
+    titulo.innerHTML = `Quem somos`;
+    texto.innerHTML = `Somos uma empresa de tecnologia fundada por 5 alunos de uma faculdade de tecnologia de São Paulo.<br>
+    O principal objetivo de nossa empresa é gerar um ambiente confortavel para seus funcionarios para que assim consigam desempenhar suas funções com máximo de eficiência.`;
+}
+
+function apresentarHardware(){
+    window.location = `index.html#bg`;
+    titulo.innerHTML = `Nosso hardware`;
+    texto.innerHTML = `<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Arduino-uno-perspective-transparent.png/1200px-Arduino-uno-perspective-transparent.png" style="width: 100%"><br>
+    Nosso Hardware é um potente arduino conectado a sensores super sensiveis capazes de medir grandes áreas, além de ser de fácil instalação e manutenção.`;
+}
+
+function apresentarSoftware(){
+    window.location = `index.html#bg`;
+    titulo.innerHTML = `Nosso software`;
+    texto.innerHTML = `<img src="CSS/IMGS/print graficos.PNG" style="width: 100%"><br>
+    Com nosso software você poderá acompanhar e verificar a temperatura e luminosidade de todas as salas da sua empresa em tempo real diretamente em nossa dashboard.`;
+}
